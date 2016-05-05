470f45f0363142a62b1598c447dae41eb39c1927
graboskyc <chris@grabosky.net>
2016-05-05 14:10:59 -0400

    Testing a new test

--- tests/sandbox_tests.py
+++ tests/sandbox_tests.py
@@ -4,9 +4,40 @@ import json
 from HTMLParser import HTMLParser
 import os
 
-class SandboxTests(TestCase):
+foundSearchString = False
+needleNode = "h1"
+needleCompare = "Hello World!"
+needle = ""
 
+# parse out what's in the source code
+class TestDocParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global foundSearchString
+        global needleNode
+        global needle
+    
+        if tag.lower().strip() == needleNode:
+            foundSearchString = True
+        else:
+            foundSearchString = False
+    def handle_data(self, data):
+        global foundSearchString
+        global needleNode
+        global needle
+        
+        if foundSearchString:
+            foundSearchString = False
+            needle = data
+
+class SandboxTests(TestCase):
     def example_test(self):
+        global foundSearchString
+        global needleNode
+        global needle
+        
+        ########################################
+        # pull data from encrypted travis keys:
+        ########################################
         serverString = os.environ["SERVERSTRING"]
         blueprintID = os.environ["BLUEPRINTID"]
         authUn = os.environ["AUTHUN"]
@@ -14,4 +45,80 @@ class SandboxTests(TestCase):
         authDom = os.environ["AUTHDOM"]
         webServerName = os.environ["WEBSERVERNAME"]
         
-        pass
+        sandboxID = ""
+
+        ########################################
+        # two part request: get auth token, then reserve
+        ########################################
+
+        # authentication
+        URI = serverString+"/login"
+        auth = {"username":authUn,"password":authPw,"domain":authDom}
+        headers = {"Content-Type":"application/json"}
+        ar = requests.put(URI, data=json.dumps(auth), headers=headers)
+        token = str(ar.content).replace('"','')
+
+        # reserve sandbox
+        body = {}
+        body["duration"] = "PT30M"  # ISO 8601 for 30 minutes
+        body["name"] = "Triggered test from Travis" # Name of sandbox
+
+        headers = {}
+        headers["Content-Type"] = "application/json"
+        headers["Accept"] = "application/json"
+        headers["Authorization"] = "Basic "+token
+
+        URI = serverString+"/v1/blueprints/"+blueprintID+"/start"
+
+        # sandbox start request
+        sbsr = requests.post(URI, data=json.dumps(body), headers=headers)
+
+        quickMap = {}
+        sbsrobj = json.loads(sbsr.text)
+
+        for component in sbsrobj["components"]:
+            quickMap[component["name"]] = component
+
+        sandboxID = sbsrobj["id"]
+
+        ########################################
+        # begin test
+        ########################################
+        # convert server name to IP since not in response?
+        testURL = os.environ["TESTURL"]
+        tr = requests.get(testURL)
+        testHTML = tr.text
+
+        parser = TestDocParser()
+        parser.feed(testHTML)
+            
+
+        ########################################
+        # end sandbox
+        ########################################
+        headers = {}
+        headers["Content-Type"] = "application/json"
+        headers["Accept"] = "application/json"
+        headers["Authorization"] = "Basic "+token
+
+        URI = serverString+"/v1/sandboxes/"+sandboxID+"/stop"
+
+        # sandbox stop request
+        sbsr = requests.post(URI, headers=headers)
+
+        ########################################
+        # do pass fail
+        ########################################
+        # compare value we got back
+        if (needle.strip() == needleCompare.strip()):
+            # identical
+            print "PASS! Expected '" + needleCompare.strip() +"' and got '"+needle.strip()+"'"
+            pass
+        elif (len(needle.strip()) == 0):
+            # couldnt find node
+            print "FAIL! Expected '" + needleCompare.strip() +"' and could not locate node '"+needleNode+"'"
+            exit(1)
+        else:
+            # not identical
+            print "FAIL! Expected '" + needleCompare.strip() +"' and got '"+needle.strip()+"'"
+            exit(1)
